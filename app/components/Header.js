b8d7b44dce501c102d7e32a200fdabe757bf921d
mahendra07limanpure <shashanklimanpure07@gmail.com>
2025-02-23 00:13:14 +0530

    Initial contract setup and Trading token UI

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,23 @@
-function Header() {
-    return (
-      <header>
-        <p className="brand">Fun.Pump</p>
-        <button>[ CONNECT ]</button>
-      </header>
-    );
+import { ethers } from "ethers"
+
+function Header({ account, setAccount }) {
+  const connectHandler = async () => {
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+    const account = ethers.getAddress(accounts[0])
+    setAccount(account);
   }
-  
-  export default Header;
+
+  return (
+    <header>
+      <p className="brand">fun.pump</p>
+
+      {account ? (
+        <button onClick={connectHandler} className="btn--fancy">[ {account.slice(0, 6) + '...' + account.slice(38, 42)} ]</button>
+      ) : (
+        <button onClick={connectHandler} className="btn--fancy">[ connect ]</button>
+      )}
+    </header>
+  );
+}
+
+export default Header;
--- app/components/LIst.js
+++ app/components/LIst.js
@@ -1,21 +1,28 @@
-function List({ toggleCreate }) {
-    return (
-      <div className="list">
-        <h2>List New Token</h2>
-  
-        <form action="">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" />
-  
-          <label htmlFor="ticker">Ticker</label>
-          <input type="text" id="ticker" />
-  
-          <input type="submit" value="[ CREATE ]" />
-        </form>
-  
-        <button onClick={toggleCreate}>[ Cancel ]</button>
-      </div>
-    );
+import { ethers } from "ethers"
+
+function List({ toggleCreate, fee }) {
+  async function listHandler(form) {
+    console.log(form.get("name"))
+    console.log(form.get("ticker"))
   }
-  
-  export default List;
+
+  return (
+    <div className="list">
+      <h2>list new token</h2>
+
+      <div className="list__description">
+        <p>fee: {ethers.formatUnits(fee, 18)} ETH</p>
+      </div>
+
+      <form action={listHandler}>
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="ticker" placeholder="ticker" />
+        <input type="submit" value="[ list ]" />
+      </form>
+
+      <button onClick={toggleCreate} className="btn--fancy">[ cancel ]</button>
+    </div>
+  );
+}
+
+export default List;
