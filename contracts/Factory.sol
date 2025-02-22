c5769a28ceffd94d8a870164bdb233c492719892
mahendra07limanpure <shashanklimanpure07@gmail.com>
2025-02-22 17:22:32 +0530

    setup token buying

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -3,20 +3,40 @@ pragma solidity 0.8.28;
 
 import {Token} from "./Token.sol";
 
+import "hardhat/console.sol";
+
 contract Factory {
+    uint256 public constant TARGET = 1 ether;
+    uint256 public constant TOKEN_LIMIT = 600_000 ether;
     uint256 public immutable fee;
     address public owner;
 
-     mapping(address => address[]) public creatorToTokens;
+    mapping(address => address[]) public creatorToTokens;
+    mapping(address => TokenSale) public tokenToSale;
 
-    event TokenCreated(address indexed token);
+    struct TokenSale {
+        uint256 sold;
+        uint256 raised;
+        bool isOpen;
+    }
+
+    event Created(address indexed token);
+    event Buy(address indexed token, uint256 amount);
+    event Sell(address indexed token, uint256 amount);
 
     constructor(uint256 _fee) {
         fee = _fee;
         owner = msg.sender;
     }
 
-  function create(
+    function getCost(uint256 _sold) public pure returns (uint256) {
+        uint256 floor = 0.001 ether;
+        uint256 step = 0.001 ether;
+        uint256 cost = (step * (_sold / 10 ** 18)) + floor;
+        return cost;
+    }
+
+    function create(
         string memory _name,
         string memory _symbol
     ) external payable {
@@ -31,15 +51,49 @@ contract Factory {
             1_000_000 ether
         );
 
+        // Create the sale.
+        TokenSale memory sale = TokenSale(0, 0, true);
+
+        // Save who created the token.
         creatorToTokens[msg.sender].push(address(token));
 
-        emit TokenCreated(address(token));
+        // Save the sale to mapping.
+        tokenToSale[address(token)] = sale;
+
+        emit Created(address(token));
     }
 
-    function buy(address _token, uint256 _amount) external payable {}
+    function buy(address _token, uint256 _amount) external payable {
+        TokenSale storage sale = tokenToSale[_token];
+
+        require(sale.isOpen == true, "Factory: Buying closed");
+        require(_amount <= 100 ether, "Factory: Amount Exceeded");
+
+        // Calculate the price of 1 token based on the total bought.
+        uint256 cost = getCost(sale.sold);
+
+        // Determine the total price for X amount.
+        uint256 price = cost * (_amount / 10 ** 18);
+
+        // Check to ensure enough ETH is sent.
+        require(msg.value >= price, "Factory: Insufficient ETH received");
+
+        // Update contract states.
+        sale.sold += _amount;
+        sale.raised += msg.value;
+
+        // If we have reached our ETH goal or buy limit, stop allowing buys.
+        if (sale.sold >= TOKEN_LIMIT || sale.raised >= TARGET) {
+            sale.isOpen = false;
+        }
+
+        // Transfer tokens to buyer.
+        Token(_token).transfer(msg.sender, _amount);
+
+        emit Buy(_token, _amount);
+    }
 
     function sell(address _token, uint256 _amount) external {}
-
 
     function withdraw() external {}
 }
